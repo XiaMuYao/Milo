96a3db0d91926bd65184f46b1cb64e60bc8d2c59
沐尧 <xiamuyaodqx@gmail.com>
2023-02-11 02:35:19 +0800

    开启k2 kotlin 编译器 并且升级 kotlin 版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,13 @@
 plugins {
     id("com.android.application") version "7.4.1" apply false
     id("com.android.library") version "7.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
 
 }
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
+}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.useK2 = true
 }
